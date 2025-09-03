1b4b2354e9937f89306dcf4573c07ee3ca0cc1cf
Aaditya Saxena <aadityasaxena76@gmail.com>
2025-09-03 20:13:23 +0530

    Add files via upload

---
+++ Day13_2.c
@@ -0,0 +1,16 @@
+//Q26 Write a program to print numbers from 1 to n.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &n) != 1 || n < 1) return 0;
+
+    printf("Numbers from 1 to %d are:\n", n);
+    for(int i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+
+    getchar();
+    getchar();
+    return 0;
+}
