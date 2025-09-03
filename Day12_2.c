fe7ad10ae0423a579d3c52037ece7ebfb2ed14f0
Aaditya Saxena <aadityasaxena76@gmail.com>
2025-09-03 19:59:08 +0530

    Update Day12_2.c

--- Day12_2.c
+++ Day12_2.c
@@ -1,22 +1,22 @@
 //Q24 Write a program to calculate an electricity bill based on units consumed.
 #include <stdio.h>
-#include <stdio.h>
-
 int main() {
     int units;
     float bill = 0;
     printf("Enter the number of units consumed: ");
     if (scanf("%d", &units) != 1) return 0;
 
-    if(units <= 50) {
+    if(units <= 100) {
         bill = units * 5;
-    } else if(units <= 150) {
-        bill = 50 * 5 + (units - 50) * 7;
+    } else if(units <= 200) {
+        bill = 100 * 5 + (units - 100) * 7;
+    } else if(units <= 300) {
+        bill = 100 * 5 + 100 * 7 + (units - 200) * 10;
     } else {
-        bill = 50 * 5 + 100 * 7 + (units - 150) * 12.5;
+        bill = 100 * 5 + 100 * 7 + 100 * 10 + (units - 300) * 12;
     }
 
-    printf("the Electricity Bill is: Rs.%.2f\n", bill);
+    printf("The Electricity Bill is: Rs.%.2f\n", bill);
 
     getchar();
     getchar();
