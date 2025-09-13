50bfe86043196db6bde7c57f00b1b2641765877c
Aaditya Saxena <aadityasaxena76@gmail.com>
2025-09-13 13:30:11 +0530

    Add files via upload

---
+++ Day23_2.c
@@ -0,0 +1,19 @@
+//Q46 Write a program to print the following pattern:
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    getchar();
+    getchar();
+    return 0;
+}
---
+++ Day24_1.c
@@ -0,0 +1,19 @@
+//Q47 Write a program to print the following pattern:
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    getchar();
+    getchar();
+    return 0;
+}   
